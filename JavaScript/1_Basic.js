c88143c72032ae038b23898f943e78e98669c011
Adarsh Verma <adarshverma549@gmail.com>
2023-01-22 22:39:06 +0530

    update

---
+++ JavaScript/1_Basic.js
@@ -0,0 +1,38 @@
+/*  JavaScript :- Basic */
+
+console.log("the world is my first programmig Language")
+
+let x = 22
+x = 24              // let x = 24 not redefined but value change 
+var y = 67      
+var y = 60          // redefine & value changed
+const z = 'Adarsh'  // not change any 
+console.log(x, y, z)
+
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("549") + BigInt("4")
+let e = 'Harry'
+let f = Symbol("I am a Adarsh Verma")
+let g = undefined
+console.log(a, b, c, d, e, f, g)
+console.log(typeof a, typeof b,  typeof c,  typeof d,  typeof e, typeof f,  typeof g)
+
+// This is Object in JavaScript. (not Dictionary)
+const item = {
+    'harry':'verma',
+    'adarsh':false,
+    'lovish':67,
+    'rohan':undefined
+}
+item['harry'] = 45          
+item['friends'] = 'ayush'
+
+console.log(item['harry'])  // 45
+console.log(item.harry)     // 45
+console.log(item.friends)   // ayush
+console.log(item.ravi)      // undefined
+
+
+
