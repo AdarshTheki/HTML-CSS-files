c9f681685a938d6f3cd7b424be9ebda2965d96ce
Adarsh Verma <adarshverma549@gmail.com>
2023-02-01 16:09:04 +0530

    explain DOM

---
+++ JavaScript/10-DOM.js
@@ -0,0 +1,17 @@
+
+// window: DOM BOM
+window.console.log(window)
+console.log(document.body)
+
+/*Document Object Model (DOM): 
+DOM represent the page contect HTML (document.body.style.background="") */
+
+document.body.style.background="red" // change page background to red
+
+/* Brouser Object Model(BOM):
+It is represent addtional object provideed by the browser(host envirement) for working with everthing except the document.
+the function alert/confirm/prompt are also a part of BOM  */
+
+location.href = "https://myworlds.com"  // redirect to another URL.
+
+
