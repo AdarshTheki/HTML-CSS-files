aa799ae22136499c3d631efe9fd714d8ce9b6d03
Adarsh Verma <adarshverma549@gmail.com>
2023-06-05 08:45:28 +0530

    commit

---
+++ JavaScript/Basic/1.js
@@ -0,0 +1,18 @@
+function double (number){
+  return 5 * number;
+} 
+console.log("value", double(2))
+console.log("value", double(2))
+console.log("value", double(3))
+console.log("value", double(3))
+
+function double1(number){
+  let extraNumber = 0
+  extraNumber++;
+  const result = 2 * number + extraNumber;
+  return result; 
+}
+console.log("value1:", double(2))
+console.log("value1:", double(2))
+console.log("value1:", double(3))
+console.log("value1:", double(3))
